ce6203086aa5162352e8930e4a290ae22150aabf
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-03 12:06:12 -0300

    feat: 357. Time to practice: solution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EditEventPage from "./pages/EditEvent";
+import EventDetailPage from "./pages/EventDetail";
+import EventsPage from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+
 // Challenge / Exercise
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
@@ -20,8 +29,28 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/pages/EventDetail.js
@@ -0,0 +1,12 @@
+import { Link, useParams } from "react-router-dom";
+
+export default function EventDetailPage() {
+  const params = useParams();
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
+      <Link to="edit">Edit</Link>
+    </>
+  );
+}
