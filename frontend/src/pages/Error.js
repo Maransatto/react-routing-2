c1b6c06f7f4d3a02d8d44f9505226a96473fed2e
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-04 13:01:45 -0300

    feat: 367. Extracting Error Data & Throwing Responses

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,12 +1,29 @@
-import { useLoaderData } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
 
 export default function ErrorPage() {
-  //   const data = useLoaderData();
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found!";
+    message = "Could not find resource or page.";
+  }
 
   return (
     <>
-      <h1>An error occurred!</h1>
-      {/* <p>{data.message}</p> */}
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   );
 }
