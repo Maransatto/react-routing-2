04598169fb142ac6e4e4ce41d42905c1f1f90c66
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-04 15:30:56 -0300

    feat: 372. Working with action() functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -48,7 +48,7 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
           {
             path: ":eventId",
             id: "event-detail",
