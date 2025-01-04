9bd661542527c34533268e5fc7b25be593c2592a
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-04 12:52:30 -0300

    feat: 366. Error handling with custom errors

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+
+export default function ErrorPage() {
+  //   const data = useLoaderData();
+
+  return (
+    <>
+      <h1>An error occurred!</h1>
+      {/* <p>{data.message}</p> */}
+    </>
+  );
+}
